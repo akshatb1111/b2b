06c28febb8021df7fa3670e419c0efa0c8ff4532
akshatb1111 <akshatb1111@gmail.com>
2024-11-20 14:39:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ function toggleStep(stepNumber) {
   var step = document.getElementById("step-" + stepNumber);
   step.classList.toggle("open");
 }
-
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
